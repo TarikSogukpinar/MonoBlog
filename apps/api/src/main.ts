9a801b938c797b6aaebf5981867b7eab2bd187a7
TarıkSogukpinar <tariksogukpinar@outlook.com>
2024-02-15 12:42:25 +0300

    chore(auth & database) modules created

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(5000);
+  app.use(helmet());
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
